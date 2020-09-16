0b2d68821b126cae0293b6cda8e79eb1d5fc8064
Sheng <shengwanhui@login2.ufhpc>
2020-09-15 22:10:02 -0400

    added some data

--- sys_info_2.sh
+++ sys_info_2.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # Program to output a system information page
 
+CURRENT_TIME="$(date +"%x %r %Z")"
+TIMESTAMP="Generated $CURRENT_TIME, by $USER"
+
 Title="System information of $USER"
+cat << _EOF_
 echo "<html>
 	<head>
 	<title>$Title</title>
 	</head>
 	<body>
 	<h1>$Title</h1>
+	<p>$TIMESTAMP</P>
 	</body>
 	</html>"
+_EOF_
